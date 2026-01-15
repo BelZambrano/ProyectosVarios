010ae3de7616d5e5535a196a802bc2bf84c8962b
BelZambrano <bzambrano888@gmail.com>
2026-01-15 14:47:41 -0300

    limpieza del proyecto, solo recordatorioclicks

--- valores.py
+++
@@ -1,9 +0,0 @@
-# Crear variables y asignar valores por el usuario
-a = int(input("Ingresa un número para a: "))
-b = int(input("Ingresa un número para b: "))
-
-# Sumar
-suma = a + b
-
-# Mostrar resultado
-print("El resultado es:", suma)
